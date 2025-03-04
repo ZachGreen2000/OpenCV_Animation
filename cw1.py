4eed6d02d63c89fd5aa86efbb18a092a02bd257a
GodKiller-21 <147402682+ZachGreen2000@users.noreply.github.com>
2025-03-04 21:37:12 +0000

    almost complete first frame

--- cw1.py
+++ cw1.py
@@ -35,9 +35,11 @@ class background:
         cv2.circle(img, (900, 10), 100, (200, 200, 200), -1)
         #floor
         cv2.rectangle(img, (0,500), (1024,1024), (100, 100, 100), -1)
+        cv2.rectangle(img, (0,500), (1024,900), (60, 60, 60), -1)
+        cv2.putText(img, "Zach Green", (820, 50), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 0))
 
 class buildings():
-    def __init__(self, x, y, scale, color):
+    def __init__(self, x, y, scale, color,):
         self.x = x
         self.y = y
         self.scale = scale
@@ -64,8 +66,37 @@ class buildings():
         #door
         cv2.rectangle(img, (self.x + int(13 * self.scale), self.y + int(50 * self.scale)), (self.x + int(22 * self.scale), self.y + int(65 * self.scale)), (100, 0 , 0), -1)
 
+class star():
+    def __init__(self, pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8, pt9, pt10, color, scale):
+        self.pt1 = pt1
+        self.pt2 = pt2
+        self.pt3 = pt3
+        self.pt4 = pt4
+        self.pt5 = pt5
+        self.pt6 = pt6
+        self.pt7 = pt7
+        self.pt8 = pt8
+        self.pt9 = pt9
+        self.pt10 = pt10
+        self.color = color
+        self.scale = scale
+
+    def draw_star(self, img):
+        points = np.array([[self.pt1], [self.pt2], [self.pt3], [self.pt4], [self.pt5], [self.pt6], [self.pt7], [self.pt8], [self.pt9], [self.pt10]], np.int32)
+        cv2.fillPoly(img, [points * self.scale], self.color)
+
 #drawing background
 background(frame1)
+#store stars
+stars = [
+    star((3,13), (5,10), (2,8), (6,8), (7,5), (8,8), (12,8), (9,10), (11,13), (7,11), color=(240, 240, 240), scale=8),
+    star((53,13), (55,10), (52,8), (56,8), (57,5), (58,8), (62,8), (59,10), (61,13), (57,11), color=(240, 240, 240), scale=5),
+    star((33,13), (35,10), (32,8), (36,8), (37,5), (38,8), (42,8), (39,10), (41,13), (37,11), color=(240, 240, 240), scale=3),
+    star((83,13), (85,10), (82,8), (86,8), (87,5), (88,8), (92,8), (89,10), (91,13), (87,11), color=(240, 240, 240), scale=8)
+]
+#draw stars
+for st in stars:
+    st.draw_star(frame1)
 #store buildings
 building = [
     buildings(350, 160, scale=10, color = (50, 50 , 50)),
