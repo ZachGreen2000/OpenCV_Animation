42ea34815ed9f957a5905541cd28a3dc640c4974
GodKiller-21 <147402682+ZachGreen2000@users.noreply.github.com>
2025-03-04 19:06:17 +0000

    buildings prefab

--- cw1.py
+++ cw1.py
@@ -13,9 +13,9 @@ class initial_stickmen:
 
     def draw(self, img):
         #head
-        cv2.circle(img, (self.x, self.y), (int(20 * self.scale)), self.color, self.thickness)
+        cv2.circle(img, (self.x, self.y), (int(20 * self.scale)), self.color, -1)
         #body
-        cv2.rectangle(img, (self.x - int(10 * self.scale), self.y + int(20 * self.scale)), (self.x + int(10 * self.scale), self.y + int(60 * self.scale)), self.color, self.thickness)
+        cv2.rectangle(img, (self.x - int(10 * self.scale), self.y + int(20 * self.scale)), (self.x + int(10 * self.scale), self.y + int(60 * self.scale)), self.color, -1)
         #arms
         arm_left = (self.x - int(30 * self.scale), self.y + int(30 * self.scale))
         arm_right = (self.x + int(30 * self.scale), self.y + int(30 * self.scale))
@@ -31,13 +31,50 @@ class background:
     def __init__(self, img):
         #block colour
         cv2.rectangle(img, (0,0), (1024,1024), (255, 0, 0), 1024)
+        #sun
+        cv2.circle(img, (1024, 0), 50, (0, 0, 255), 300)
+
+class buildings():
+    def __init__(self, x, y, scale, color):
+        self.x = x
+        self.y = y
+        self.scale = scale
+        self.color = color
+
+    def buildings(self, img):
+        #buildings
+        cv2.rectangle(img, (self.x, self.y), (self.x + int(35 * self.scale), self.y + int(65 * self.scale)), self.color, -1)
+        #windows
+        cv2.rectangle(img, (self.x + int(5 * self.scale), self.y + int(5 * self.scale)), (self.x + int(12 * self.scale), self.y + int(12 * self.scale)), (255, 222 , 0), -1)
+        cv2.rectangle(img, (self.x + int(23 * self.scale), self.y + int(5 * self.scale)), (self.x + int(30 * self.scale), self.y + int(12 * self.scale)), (255, 222 , 0), -1)
+        #layer2
+        cv2.rectangle(img, (self.x + int(5 * self.scale), self.y + int(15 * self.scale)), (self.x + int(12 * self.scale), self.y + int(22 * self.scale)), (255, 222 , 0), -1)
+        cv2.rectangle(img, (self.x + int(23 * self.scale), self.y + int(15 * self.scale)), (self.x + int(30 * self.scale), self.y + int(22 * self.scale)), (255, 222 , 0), -1)
+        #layer3
+        cv2.rectangle(img, (self.x + int(5 * self.scale), self.y + int(25 * self.scale)), (self.x + int(12 * self.scale), self.y + int(32 * self.scale)), (255, 222 , 0), -1)
+        cv2.rectangle(img, (self.x + int(23 * self.scale), self.y + int(25 * self.scale)), (self.x + int(30 * self.scale), self.y + int(32 * self.scale)), (255, 222 , 0), -1)
+        #layer4
+        cv2.rectangle(img, (self.x + int(5 * self.scale), self.y + int(35 * self.scale)), (self.x + int(12 * self.scale), self.y + int(42 * self.scale)), (255, 222 , 0), -1)
+        cv2.rectangle(img, (self.x + int(23 * self.scale), self.y + int(35 * self.scale)), (self.x + int(30 * self.scale), self.y + int(42 * self.scale)), (255, 222 , 0), -1)
+        #layer5
+        cv2.rectangle(img, (self.x + int(5 * self.scale), self.y + int(45 * self.scale)), (self.x + int(12 * self.scale), self.y + int(52 * self.scale)), (255, 222 , 0), -1)
+        cv2.rectangle(img, (self.x + int(23 * self.scale), self.y + int(45 * self.scale)), (self.x + int(30 * self.scale), self.y + int(52 * self.scale)), (255, 222 , 0), -1)
+        #door
+
+
 #drawing background
 background(frame1)
+#store buildings
+building = [
+    buildings(50, 200, scale=10, color = (0, 0 , 0))
+]
+#drawing buildings
+for build in building:
+    build.buildings(frame1)
 #array for stickmen storage
 stickmen = [
-    initial_stickmen(200, 200, scale=5, color=(0, 255, 0), thickness=1)
+    initial_stickmen(200, 800, scale=2, color=(0, 255, 0), thickness=4)
 ]
-
 #draw stickmen
 for stickman in stickmen:
     stickman.draw(frame1)
